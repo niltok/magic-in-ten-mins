7329c8e068576e3bdcbbae4cc90e6410240c432f
niltok <niltok@163.com>
2021-01-28 18:40:37 +0800

    fix(style) line height

--- gen.js
+++ gen.js
@@ -17,10 +17,10 @@ const title = $$('title')('十分钟魔法练习')
 const hljscss = '<link href="https://cdn.bootcdn.net/ajax/libs/highlight.js/10.3.2/styles/a11y-light.min.css" rel="stylesheet">'
 const materialize = '<link href="https://cdn.bootcdn.net/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css" rel="stylesheet">'
 
-const sbody = 'body { max-width: 650px; margin: auto; width: 90%; margin-top: 10%; margin-bottom: 10%; color: #0B0E26; background: #FAFAFC; }\n'
+const sbody = 'body { max-width: 650px; margin: auto; width: 90%; margin-top: 10%; margin-bottom: 10%; color: #0B0E26; background: #FAFAFC; line-height: 2em; }\n'
 const sfont = 'body { font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Microsoft YaHei,Source Han Sans SC,Noto Sans CJK SC,WenQuanYi Micro Hei,sans-serif; }\n'
-const sh1 = 'h1 { font-size: 2.5em; color: #EF96AB; }\n'
-const sh2 = 'h2 { margin-top: 2em; }\n'
+const sh1 = 'h1 { font-size: 2.5em; color: #EF96AB; line-height: 1.5em; }\n'
+const sh2 = 'h2 { margin-top: 2em; line-height: 1.5em; }\n'
 const scenter = 'h1, h2, h3 { text-align: center; }\n'
 const squote = 'blockquote { color: gray; margin: 0; padding: 1 1 1 20; border-left: 5px solid #EF96AB; }\n'
 const scode = 'code, pre { font-family: Consolas,Menlo,Monaco,source-code-pro,Courier New,monospace; background: #FCF6FC; }\n'
